1c36e130705f48f552dbc1522df2e3bd047316ef
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-24 16:54:55 -0300

    fix: add missing SSM parameters for video SQS, SNS topic, and bucket details

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,19 @@ data "aws_ssm_parameter" "cognito_user_pool_id" {
 data "aws_ssm_parameter" "cognito_client_id" {
   name = "/soat10/authorization-api/cognito_client_id"
 }
+
+data "aws_ssm_parameter" "video_sqs_id" {
+  name = "/soat10/infra-video-manager/video_sqs_id"
+}
+
+data "aws_ssm_parameter" "video_sns_topic_arn" {
+  name = "/soat10/infra-video-manager/video_sns_topic_arn"
+}
+
+data "aws_ssm_parameter" "video_bucket_name" {
+  name = "/soat10/infra-video-manager/video_bucket_name"
+}
+
+data "aws_ssm_parameter" "video_bucket_arn" {
+  name = "/soat10/infra-video-manager/video_bucket_arn"
+}
