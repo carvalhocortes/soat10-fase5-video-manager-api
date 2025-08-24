be8323eb0d3fda51eaf36f3dfc9f42dcd25340b6
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-24 10:55:34 -0300

    fix: update role variable naming and use SSM parameters for Cognito IDs in lambda functions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,13 +4,13 @@ resource "aws_lambda_function" "upload_files" {
   source_code_hash = filebase64sha256("../lambda.zip")
   handler          = "index.uploadFilesHandler"
   runtime          = "nodejs22.x"
-  role             = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
+  role             = "arn:aws:iam::${var.AWS_ACCOUNT_ID}:role/LabRole"
   timeout          = 10
 
   environment {
     variables = {
-      COGNITO_USER_POOL_ID = var.COGNITO_USER_POOL_ID
-      COGNITO_CLIENT_ID    = var.COGNITO_CLIENT_ID
+      COGNITO_USER_POOL_ID = data.aws_ssm_parameter.cognito_user_pool_id.value
+      COGNITO_CLIENT_ID    = data.aws_ssm_parameter.cognito_client_id.value
     }
   }
 }
@@ -21,13 +21,13 @@ resource "aws_lambda_function" "download_files" {
   source_code_hash = filebase64sha256("../lambda.zip")
   handler          = "index.downloadFilesHandler"
   runtime          = "nodejs22.x"
-  role             = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
+  role             = "arn:aws:iam::${var.AWS_ACCOUNT_ID}:role/LabRole"
   timeout          = 10
 
   environment {
     variables = {
-      COGNITO_USER_POOL_ID = var.COGNITO_USER_POOL_ID
-      COGNITO_CLIENT_ID    = var.COGNITO_CLIENT_ID
+      COGNITO_USER_POOL_ID = data.aws_ssm_parameter.cognito_user_pool_id.value
+      COGNITO_CLIENT_ID    = data.aws_ssm_parameter.cognito_client_id.value
     }
   }
 }
@@ -38,13 +38,13 @@ resource "aws_lambda_function" "list_files" {
   source_code_hash = filebase64sha256("../lambda.zip")
   handler          = "index.listFilesHandler"
   runtime          = "nodejs22.x"
-  role             = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
+  role             = "arn:aws:iam::${var.AWS_ACCOUNT_ID}:role/LabRole"
   timeout          = 10
 
   environment {
     variables = {
-      COGNITO_USER_POOL_ID = var.COGNITO_USER_POOL_ID
-      COGNITO_CLIENT_ID    = var.COGNITO_CLIENT_ID
+      COGNITO_USER_POOL_ID = data.aws_ssm_parameter.cognito_user_pool_id.value
+      COGNITO_CLIENT_ID    = data.aws_ssm_parameter.cognito_client_id.value
     }
   }
 }
